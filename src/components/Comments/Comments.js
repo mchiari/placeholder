910a254749db5fda0b9b291e23f9578facde2804
mchiari <mchiaridev@gmail.com>
2022-03-09 10:35:58 -0300

    primeira apresentação

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { getAllComments, getComments } from '../../features/cardSlice';
+import { getAllComments } from '../../features/cardSlice';
 import './Comments.css';
 
 const Comments = (props) => {
 	const prop = props;
-	const comments = useSelector(getComments);
 	const allComments = useSelector(getAllComments);
-	console.log(allComments);
-
-	// const fiveComments = allComments.includes()
+	// console.log(allComments);
 
 	const fiveComments = allComments.filter(function (el) {
 		return el.postId === prop.postId;
 	});
 
-	console.log(fiveComments);
-
-	// console.log(allComments[prop.postId]?.body);
+	// console.log(fiveComments);
 
 	const mapComments = fiveComments.map((el, i) => {
 		return (
@@ -29,32 +24,6 @@ const Comments = (props) => {
 	});
 
 	return <div>{mapComments}</div>;
-
-	//SHOW ONLY ONE COMMENT FROM STATE
-	// return (
-	// 	<div>
-	// 		<div className='comment'>
-	// 			<div className='comment-user'>{comments[0]?.email} </div>
-	// 			<div className='comment-comment'>{comments[0]?.body}</div>
-	// 		</div>
-	// 		<div className='comment'>
-	// 			<div className='comment-user'>{comments[1]?.email} </div>
-	// 			<div className='comment-comment'>{comments[1]?.body}</div>
-	// 		</div>
-	// 		<div className='comment'>
-	// 			<div className='comment-user'>{comments[2]?.email} </div>
-	// 			<div className='comment-comment'>{comments[2]?.body}</div>
-	// 		</div>
-	// 		<div className='comment'>
-	// 			<div className='comment-user'>{comments[3]?.email} </div>
-	// 			<div className='comment-comment'>{comments[3]?.body}</div>
-	// 		</div>
-	// 		<div className='comment'>
-	// 			<div className='comment-user'>{comments[4]?.email} </div>
-	// 			<div className='comment-comment'>{comments[4]?.body}</div>
-	// 		</div>
-	// 	</div>
-	// );
 };
 
 export default Comments;
